401932ff32761d5001d63718341ec2b114353cdf
Joe-Zhang <4940034@qq.com>
2016-03-06 19:49:47 +0800

    Update MinYi.py

--- MinYi.py
+++ MinYi.py
@@ -1,13 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import pymongo
 
-for a in range(1,27300,500):
-    b = a + 499
-    url = "http://minyi.runsky.com/j8DJ6.php?action=1&tmod=1&startv=1&endv=10&startv={}&endv={}&tmod=1 ".format(a,b)
-    wb_date = requests.get(url)
-    soup = BeautifulSoup(wb_date.text,'html.parser').get_text()
-    # print(soup)
-    out = json.loads(soup)['resault_array'] #LOL the programmer at MinYi cannot spell "result"
-    for i in out:
-        print(i['id'],i['title'],'\n',i['reply'],'\n')
+
+client = pymongo.MongoClient('localhost',27017)
+MinYiApp = client['MinYiApp']
+data = MinYiApp['data']
+
+def crawl():
+    for a in range(1,99999,200):
+        b = a + 199
+        url = "http://minyi.runsky.com/j8DJ6.php?action=1&tmod=1&startv=1&endv=10&startv={}&endv={}&tmod=1 ".format(a,b)
+        wb_date = requests.get(url)
+        soup = BeautifulSoup(wb_date.text,'html.parser').get_text()
+        # print(soup)
+        out = json.loads(soup)['resault_array']
+        for i in out:
+            if ((data.find_one({"id":i['id']}))):
+                print("duplicate!")
+            else:
+                data.insert_one(i)
+                print(i)
+
+crawl()
